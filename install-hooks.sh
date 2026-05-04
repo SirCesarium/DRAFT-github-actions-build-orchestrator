9f7a250fc8f92875493027d2a6e09e0416eed79f
SirCesarium <cesarium3600@gmail.com>
2026-05-04 17:09:14 -0400

    ci: remove pre-push hook installation from install-hooks.sh

--- install-hooks.sh
+++ install-hooks.sh
@@ -23,7 +23,4 @@ mkdir -p .git/hooks
 cp hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
-cp hooks/pre-push .git/hooks/pre-push
-chmod +x .git/hooks/pre-push
-
 echo "Hooks installed successfully."
